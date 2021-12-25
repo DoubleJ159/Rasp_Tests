30b365559e7827f77029559413c2b393517cfdac
JBonyoma <90006723+JBonyoma@users.noreply.github.com>
2021-12-24 22:45:19 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,5 +1,16 @@
+'''This code ouputs 1 (Hi/On) to gpio pin 18 for a few seconds then 0 (Low/Off)'''
+
 import time as t
 import RPi.GPIO as g
 
 g.setmode(g.BOARD)
 g.setup(18, g.OUT)
+
+for i in range (3):
+    g.output(18, 1)
+    t.sleep(3)
+    g.output(18, 0)
+    t.sleep(2)
+    print("Done ", i)
+
+g.cleanup()
